7b4bc957a69bb185401f1b5367f1b3d195d37fed
Lukas Höppner <lukas.hoeppner@gmail.com>
2014-05-16 11:38:21 +0200

    acorn.js example

---
+++ ToolTests/acorn.js
@@ -0,0 +1,29 @@
+var acorn = require('acorn'),
+    walk  = require('acorn/util/walk.js'),
+    fs    = require('fs'),
+    pd    = require('pretty-data').pd;
+
+fs.readFile('testfile.js', 'utf8', function(err, input) {
+    var options = {
+        strictSemicolons: true
+    };
+    var result = acorn.parse(input, options);
+    // console.log(pd.json(result));
+
+    var variables = {};
+    var visitors = {
+        ExpressionStatement: function(node, state){
+            value = node.expression.right.value || variables[node.expression.right.name];
+            variables[node.expression.left.name] = value;
+        },
+        VariableDeclaration: function(node) {
+            node.declarations.forEach(function(declaration){
+                var name = declaration.id.name;
+                var value = declaration.init.value;
+                variables[name] = value;
+            });
+        }
+    };
+    walk.simple(result, visitors);
+    console.log(variables);
+});
---
+++ ToolTests/testfile.js
@@ -0,0 +1,4 @@
+var a = "blurp";
+var b = "marple";
+a = b;
+a = 3;

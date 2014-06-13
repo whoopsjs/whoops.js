023e0394bae89b80f92b085951daad90e0493c1d
Michel Zimmer <mzimmer@fb3.uni-bremen.de>
2014-06-13 14:44:48 +0200

    remove old stuff

--- MainProgram/testfile.js
+++ MainProgram/testfile.js
@@ -1,31 +1,9 @@
-// var a = "blurp";
-// var b = "marple";
-// a = b;
-// a = 3;
-// if (a === c) {
-//   a = 4;
-// } else if (b === d) {
-//   b = 3;
-// }
-// if (true) {
-//   f = g;
-// }
-//
-while (a < 5) {
-  b = a;
-  d = f;
-}
+////////////////////////////////////////////////////////////////////////////////
+//                                  DOM I/O                                   //
+////////////////////////////////////////////////////////////////////////////////
 
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-for (var item in array) {
-  item -= 1;
-}
+// TODO
 
-for (var i = 0; i < 100; i++) {
-  var j = i;
-}
-
-// a = 6;
 
 ////////////////////////////////////////////////////////////////////////////////
 //                           Direct Execution Sinks                           //
@@ -45,7 +23,7 @@ eval("console.log(\"" + userControlledValue + "\");");
 // Function Name: Function
 // Argument:      first if there's one, the last if >1 args
 // Browser:       All
-// Example:       Function("jsCode"+usercontrolledVal ) , 
+// Example:       Function("jsCode"+usercontrolledVal ) ,
 //                Function("arg","arg2","jsCode"+usercontrolledVal )
 // TODO does 'Function(String)' work this way?
 /*

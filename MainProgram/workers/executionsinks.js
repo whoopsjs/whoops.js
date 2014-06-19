d735b6798a6097049969f0649906d72d43976b87
Michel Zimmer <mzimmer@fb3.uni-bremen.de>
2014-06-19 12:03:43 +0200

    setImmediate

--- MainProgram/workers/executionsinks.js
+++ MainProgram/workers/executionsinks.js
@@ -8,35 +8,42 @@ module.exports = function (tree) {
           "type": "risk",
           "message": "using eval() is not safe",
           "weight": 1,
-          "position":
-          {
+          "position": {
             "start": node.start,
             "end": node.end
           }
         });
-      } else if(node.callee.name === 'setTimeout') {
+      } else if (node.callee.name === 'setTimeout') {
         tree.data.problems.push({
           "type": "risk",
           "message": "using setTimeout() is not safe",
           "weight": 1,
-          "position":
-          {
+          "position": {
             "start": node.start,
             "end": node.end
           }
         });
-      } else if(node.callee.name === 'setInterval') {
+      } else if (node.callee.name === 'setInterval') {
         tree.data.problems.push({
           "type": "risk",
           "message": "using setInterval() is not safe",
           "weight": 1,
-          "position":
-          {
+          "position": {
             "start": node.start,
             "end": node.end
           }
         });
-      };
+      } else if (node.callee.name === 'setImmediate') {
+        tree.data.problems.push({
+          "type": "risk",
+          "message": "using setImmediate() is not safe",
+          "weight": 1,
+          "position": {
+            "start": node.start,
+            "end": node.end
+          }
+        });
+      }
     }
   });
 };

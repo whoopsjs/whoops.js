01e401420a22abc87b11dad2df8ab0dde03c9a18
Michel Zimmer <mzimmer@fb3.uni-bremen.de>
2014-06-20 09:25:13 +0200

    " -> '

--- MainProgram/workers/executionsinks.js
+++ MainProgram/workers/executionsinks.js
@@ -5,65 +5,65 @@ module.exports = function (tree) {
     CallExpression: function (node, state, c) {
       if (node.callee.name === 'eval') {
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using eval() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using eval() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       } else if (node.callee.name === 'setTimeout') {
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using setTimeout() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using setTimeout() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       } else if (node.callee.name === 'setInterval') {
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using setInterval() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using setInterval() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       } else if (node.callee.name === 'setImmediate') {
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using setImmediate() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using setImmediate() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       } else if (node.callee.name === 'execScript') {
         // TODO seconds argument === 'JScript'
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using execScript() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using execScript() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       } else if (node.callee.type === 'MemberExpression'
                  && node.callee.object.name === 'crypto'
                  && node.callee.property.name === 'generateCRMFRequest') {
         tree.data.problems.push({
-          "type": "risk",
-          "message": "using crypto.generateCRMFRequest() is not safe",
-          "weight": 1,
-          "position": {
-            "start": node.start,
-            "end": node.end
+          'type': 'risk',
+          'message': 'using crypto.generateCRMFRequest() is not safe',
+          'weight': 1,
+          'position': {
+            'start': node.start,
+            'end': node.end
           }
         });
       }

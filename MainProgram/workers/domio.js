0da8fdc789c638cb7cb4ce1d8c5789fab1874078
Manuel <mkramer@informatik.uni-bremen.de>
2014-06-20 10:58:11 +0200

    clean up

--- MainProgram/workers/domio.js
+++ MainProgram/workers/domio.js
@@ -7,137 +7,159 @@ module.exports = function (tree) {
         && node.callee.name === 'document'){
         if(node.callee.property.name === 'getElementById'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'getElementByName'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'getElementByTagName'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'documentElement'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'images'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'domain'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'body'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Output",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Output',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'createAttribute'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Input",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'createComment'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Input",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'createDocumentFragment'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Input",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'createElement'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Input",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
         if(node.callee.property.name === 'createTextNode'){
           tree.data.problems.push({
-            "type": "warning",
-            "message": "Input",
-            "weight": 5,
-            "position": {
-              "start": node.start,
-              "end": node.end
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
+            }
+          });
+        }
+        if(node.callee.property.name === 'createTextNode'){
+          tree.data.problems.push({
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
+            }
+          });
+        }
+        if(node.callee.property.name === 'createTextNode'){
+          tree.data.problems.push({
+            'type': 'warning',
+            'message': 'Input',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
             }
           });
         }
       }
     }
-  });    //graph.domio = "success";
+  });    //graph.domio = 'success';
 };

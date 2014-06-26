4946e6e6d1c7d52e3e2eb8276a470c1ce8c2c762
Manuel <mkramer@informatik.uni-bremen.de>
2014-06-27 01:44:49 +0200

    tried to fix the search for the user input, still not working correctly i think.

--- MainProgram/workers/domio.js
+++ MainProgram/workers/domio.js
@@ -1,13 +1,15 @@
 var walk = require('acorn/util/walk');
-
+//still not working, the path trough the tree is not correct. Doesn't find the end.
 module.exports = function (tree) {
   walk.recursive(tree.data.cfg, {}, {
-    CallExpression: function (node, state, c) {
-      if(node.declarations.type === 'VariableDeclarator'
+    VariableDeclarator: function (node, state, c) {
+      if(node.init.type === 'CallExpression'
         && node.callee.name === 'prompt'){
+        var input = node.id.name;
         if(node.callee.type === 'MemberExpression'
           && node.object.name === 'document'){
-          if(node.callee.property.name === 'createTextNode'){
+          if(node.callee.property.name === 'createTextNode'
+            && node.arguments.name === input){
             tree.data.problems.push({
               'type': 'warning',
               'message': 'To put a user defined variable into the DOM tree is a risk',

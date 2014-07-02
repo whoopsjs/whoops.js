50e727f242559103719d0bb555b7d87c97450cb8
Tim Godthardt <godthtim@uni-bremen.de>
2014-07-02 11:40:05 +0200

    Updated worker, now searches through VariableDeclarators successfully.

--- MainProgram/workers/domio.js
+++ MainProgram/workers/domio.js
@@ -1,27 +1,50 @@
 var walk = require('acorn/util/walk');
-//still not working, the path trough the tree is not correct. Doesn't find the end.
+//still not working, but puts all input values into an array
+//inefficient, but will do for now.
+function contains(a, obj) {
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
 module.exports = function (tree) {
-  walk.recursive(tree.data.cfg, {}, {
-    VariableDeclarator: function (node, state, c) {
-      if(node.init.type === 'CallExpression'
-        && node.callee.name === 'prompt'){
-        var input = node.id.name;
-        if(node.callee.type === 'MemberExpression'
-          && node.object.name === 'document'){
-          if(node.callee.property.name === 'createTextNode'
-            && node.arguments.name === input){
-            tree.data.problems.push({
-              'type': 'warning',
-              'message': 'To put a user defined variable into the DOM tree is a risk',
-              'weight': 5,
-              'position': {
-                'start': node.start,
-                'end': node.end
-              }
-            });
-          }
+  var inputs = new Array(); //saving all the names of our inputs
+  inputs.push('input1');
+  walk.recursive(tree.data.cfg, {}, {  
+    //We will have to go to the VariableDeclarators over the VariableDeclarations for some reason.
+    VariableDeclaration: function (node, state, c) {
+      var declarator = node.declarations;
+      for(var i = 0; i < declarator.length; i++){
+        var subnode = declarator[i].init;
+        if(subnode.type === 'CallExpression'
+          && subnode.callee.name === 'prompt'){
+            inputs.push(declarator[i].id.name);
         }
       }
     }
+  });
+  
+  walk.recursive(tree.data.cfg, {}, {
+    CallExpression: function (node, state, c) {
+      if(node.callee.type === 'MemberExpression'
+        && node.callee.object.name === 'document'){
+        console.log(node.arguments[0]);
+        if(node.callee.property.name === 'createTextNode'
+          &&  contains(inputs,node.arguments[0].name)){
+          tree.data.problems.push({
+            'type': 'warning',
+            'message': 'To put a user defined variable into the DOM tree is a risk',
+            'weight': 5,
+            'position': {
+              'start': node.start,
+              'end': node.end
+            }
+          });
+        }
+      }      
+    } 
   });    //graph.domio = 'success';
 };

f1b214f669cb51367c7eda610a807d487011bc16
Tim Godthardt <godthtim@uni-bremen.de>
2014-07-18 08:51:06 +0200

    Now test for assignment of evil functions.
    Incomplete tracking of assigned values.

--- MainProgram/workers/domio.js
+++ MainProgram/workers/domio.js
@@ -41,17 +41,56 @@ function getLastName(a, obj){
 	return [];
 }
 
+//Gets the assigned value for an assignment
+function getAssignedValue(node){
+	console.log(node.type);
+	if(node.type === 'CallExpression'){
+		if(node.callee.name === undefined){
+			return node.callee.name;
+		}
+		else if(node.callee.property !== undefined){
+			return node.callee.property.name;
+		}
+	}
+	else if(node.type === 'Literal'){
+		return node.right.value;
+	}
+	else if(node.type === 'BinaryExpression'){
+		return getAssignedValue(node.left) 
+		+ node.right.operator 
+		+ getAssignedValue(node.right);
+	}
+	else{
+		return [];
+	}
+}
+
 //Tests whether or not a node poses a risk
 function dangerousNode(node){
   return node.callee.name === 'prompt' || 
          (node.callee.property.name === 'createElement' || 
-         node.callee.property.name === 'getElementsByTagName')
-         && contains(monitoredTags,node.arguments[0].value);
+         (node.callee.property.name === 'getElementsByTagName')
+         && contains(monitoredTags,node.arguments[0].value));
 }
 
 module.exports = function (tree) {
   var inputs = new Array(); //saving all the names of our inputs in the Form {Name, Type}
-  var assignments = new Array(); //Contains all new assigned monitored values {Name, dangerous (boolean), ending symbol}
+  var assignments = new Array(); //Contains all new assigned monitored values {Name, dangerous (boolean), value, starting symbol, ending symbol}
+	var evilFunctions = new Array();
+	
+	//called when a warning happens
+	function pushWarning(name, start, end){
+		tree.data.problems.push({
+			'type': 'warning',
+			'message': 'To put the user defined variable ' + name + ' into the DOM tree is a risk',
+			'weight': 5,
+			'position': {
+			'start': start,
+			'end': end
+			}
+		});
+	}
+	
   walk.recursive(tree.data.cfg, {}, {
     //We will have to go to the VariableDeclarators over the VariableDeclarations for some reason.
     VariableDeclaration: function (node, state, c) {
@@ -60,8 +99,7 @@ module.exports = function (tree) {
         var subnode = declarator[i].init;
         //prompts,created Elements and Elements gotten are monitored
         if(subnode.type === 'CallExpression'
-          && (subnode.callee.name === 'prompt' || 
-          dangerousNode(subnode)))
+          && dangerousNode(subnode))
           {
             var name = declarator[i].id.name; //Variable Name
             var type = subnode.arguments[0].value; //Variable type (e.g. 'input', 'span', 'button' etc.)
@@ -69,23 +107,21 @@ module.exports = function (tree) {
             //If we get Elements by Tag Name, then we only need to monitor the value,
             //if it actually returns something.
             if(subnode.callee.name !== 'prompt' 
-              &&subnode.callee.property.name === 'getElementsByTagName' 
+              && subnode.callee.property.name === 'getElementsByTagName' 
               && !contains(inputs, nameType)
               && containsType(inputs,type)){           
               //Push element into List if not already existant.              
               inputs.push(nameType);             
-              assignments.push(new Array(name, true, node.end));
+              assignments.push(new Array(name, true, getAssignedValue(subnode), node.start, node.end));
             }          
             else if(!contains(inputs, nameType)){
               inputs.push(nameType);
-              assignments.push(new Array(name, true, node.end));
+              assignments.push(new Array(name, true, getAssignedValue(subnode), node.start, node.end));
             }
         }
       }
     }
 	});
-	
-	
 	
 	walk.recursive(tree.data.cfg, {}, {
     //We test all assigned values for changes 
@@ -102,39 +138,56 @@ module.exports = function (tree) {
           dangerous = false;
         }
         //Is monitored Value newly assigned?
-        var name = node.left.name;
-        var endSign = node.end;
-        if (containsName(inputs,name)){   
-          assignments.push(new Array(name, dangerous, endSign));
+        if (containsName(inputs,node.left.name)){   
+          assignments.push(new Array(node.left.name, dangerous, getAssignedValue(node.right) ,node.start, node.end));
         }
 				//Variable takes value of another dangerous one
-				else if(node.right.type == 'Identifier' && containsName(inputs, node.right.name)){
-					assignments.push(new Array(name, getLastName(assignments, node.right.name[1]), endSign));
+				else if(node.right.type == 'Identifier' 
+				&& containsName(inputs, node.right.value)){
+					assignments.push(new Array(node.left.name, getLastName(assignments, node.right.value)[1], node.right.value ,node.start, node.end));
 				}
         //Defined variable gets dangerous value
         else if(dangerous){
-          inputs.push(new Array(name, node.right.callee.name));
-          assignments.push(new Array(name, true, endSign));
+          inputs.push(new Array(node.left.name, node.right.callee.name));
+          assignments.push(new Array(node.left.name, true, getAssignedValue(node.right) ,node.start, node.end));
         }
       }
+			//Continue search if in FunctionExpression
 			if(node.right.type === 'FunctionExpression'){
 				c(node.right.body,state);
 			}
     }
   });
+	
+	//Walk all Functions to look if evil values are behind Functions.
+	walk.recursive(tree.data.cfg, {}, {
+    FunctionDeclaration: function (node, state, c) {
+			for(var i = 0; i < assignments.length; i++){
+				if(assignments[i][3] >= node.start 
+				&& assignments[i][4] <= node.end
+				&& assignments[i][1]){
+					evilFunctions.push(node.id.name);
+				}
+			}
+		}
+	});
 
   walk.recursive(tree.data.cfg, {}, {
     CallExpression: function (node, state, c) {
       //Every appended Object will (for now) throw an error, even if the container isn't added to the body.
       if(node.arguments[0].callee !== undefined 
         && node.callee.property.name === 'appendChild'){
-        //Go into lowest CallExpression in chain
+        //Go into lowest CallExpression in chain until you reach a document call
         var subNode = node;
-        while(subNode.arguments[0].type === 'CallExpression'){
+        while(subNode.arguments[0] !== undefined && 
+				subNode.arguments[0].type === 'CallExpression' &&
+				!(subNode.callee.object !== undefined && 
+				subNode.callee.object.name === 'document')){
           subNode = subNode.arguments[0];
         }
         //Go through the different chain design to find variable name.
-        if(subNode.callee.object.name === 'document'){
+        if(subNode.callee.object !== undefined && 
+				subNode.callee.object.name === 'document'){
           var name;
           //Name is very early within chain.
           if(subNode.arguments[0].type === 'Identifier'){
@@ -148,27 +201,23 @@ module.exports = function (tree) {
             }
             name = memNode.object.name;
           }
+					else if(subNode.arguments[0].type === 'CallExpression'){
+						name = subNode.arguments[0].callee.name;
+					}
           //Last assignment will be used.
           var nowDangerous = false;
           for(var i = 0; i < assignments.length; i++){
             if(assignments[i][0] === name){
               //Assignment before the appending?
-              if(assignments[i][2] < node.end){
+              if(assignments[i][4] <= node.end){
                 nowDangerous = assignments[i][1];
               }
             }
           }
           //Push warning if dangerous value is appended
-          if(nowDangerous){
-            tree.data.problems.push({
-              'type': 'warning',
-              'message': 'To put the user defined variable ' + name + ' into the DOM tree is a risk',
-              'weight': 5,
-              'position': {
-              'start': node.start,
-              'end': node.end
-              }
-            });
+          if(nowDangerous
+					|| contains(evilFunctions,name)){
+						pushWarning(name, node.start, node.end)
           }
         }
       }
